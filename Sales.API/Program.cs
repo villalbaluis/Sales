900a08ac4c24c773d124c598d3477d267a4faaf9
LUIS-VILLALBA\User <luisvillalba244@gmail.com>
2024-02-13 02:00:21 -0500

    First test of the API working, Works on Swagger page, get and post for Countries

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));  
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
 
 var app = builder.Build();
 
@@ -22,5 +22,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 
 app.Run();
