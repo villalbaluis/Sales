23aa2832ba750d33d95bc654358093ea246f9df5
LUIS-VILLALBA\User <luisvillalba244@gmail.com>
2024-02-13 10:50:54 -0500

    Add loading gif, add CRUD to countries API, test in Swagger.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -9,25 +9,64 @@ namespace Sales.API.Controllers
     [Route("/api/countries")]
     public class CountriesController : ControllerBase
     {
+        /// Atributos referenciables a través del _ para no usar: this
         private readonly DataContext _context;
 
+        /// Instancia del DataContext de base de datos al atributo definido.
         public CountriesController(DataContext context)
         {
             _context = context;
         }
 
+        /// Metodo para obtener todos los elementos. Countries
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
         }
 
+        /// Metodo para obtener un único elemento. Countries
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        /// Metodo para crear nuevos elementos. Countries
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
             _context.Add(country);
             await _context.SaveChangesAsync();
             return Ok(country);
+        }
+
+        /// Metodo para editar elementos existentes. Countries
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        /// Metodo para eliminar un elemento. Countries
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
