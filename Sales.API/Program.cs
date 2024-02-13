72a946072e6577c606049b6293faa9c989720f6f
LUIS-VILLALBA\User <luisvillalba244@gmail.com>
2024-02-13 01:23:04 -0500

    Add dbContext, test first migration

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,15 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));  
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
